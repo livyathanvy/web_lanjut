2bbf77b8915cd7e9d2aab8c6314d90b993c64d5d
livyathanv <indy25.mhs@akb.ac.id>
2026-06-24 12:24:03 +0700

    fix vercel

--- route/user.routes.js
+++ route/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { tambahuser, login, user } from "../controllers/user.controllers.js";
+import { tambahuser, login, getAllUsers, cariuserByID } from "../controllers/user.controllers.js";
 import { authenticateToken } from "../middleware/VerifyTokens.js";
 const routerUser = express.Router();
 routerUser.post("/", tambahuser);
 routerUser.post("/login", login);
-routerUser.get("/getAllUsers", user);
+routerUser.get("/list", getAllUsers);
+routerUser.get("/:id", cariuserByID);
 routerUser.get("/dashboard", authenticateToken, (req, res) => {
   res.send("Welcome to the dashboard!");
 });
